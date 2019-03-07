8f7d09cf82370512a14b2d24d2a86cbbe0f0a235
Marcos Martins <marcos_af_martins@hotmail.com>
2019-03-07 17:42:49 -0300

    Initial commit

---
+++ src/base/app.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <h1>Olá mundo</h1>
+        )
+    }
+}
+
+export default App;
